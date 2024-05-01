dfd548acc2cbe500597b87736041b3a47d656ac7
Kwonmuang Thanthima <ktt5497@g.rit.edu>
2024-04-30 21:39:52 -0400

    Finished What I Can Do
    I will try to make this website work. In the summer.

--- client/money.jsx
+++ client/money.jsx
@@ -1,17 +1,33 @@
-const helper = require('./helper.js');
 const React = require('react');
 const {createRoot} = require('react-dom/client');
 
-const premiumWindow = () => {
+const PremiumWindow = (props) => {
+
+    const handlePreniumBuy = async () => {
+        try {
+            const response = await fetch('/premium', {
+                method: 'POST',
+                header: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username: props.query.username }),
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    //Clicking the button should enable premium
     return (
         <div>
-            
+            <button onClick={handlePreniumBuy}>Buy Prenium</button>
         </div>
     );
 };
 
 const init = () => {
     const root = createRoot(document.getElementById('content3'));
+    root.render(<PremiumWindow />);
 };
 
 window.onload = init;
